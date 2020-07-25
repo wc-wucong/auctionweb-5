df83a8ff0a282132ae71c64735f04ceb9544e55a
1661719505 <1661719505@qq.com>
2020-07-25 09:14:36 +0800

    第二次修改

--- src/main/java/com/accp/Auctionweb5Application.java
+++ src/main/java/com/accp/Auctionweb5Application.java
@@ -8,6 +8,8 @@ public class Auctionweb5Application {
 
 	public static void main(String[] args) {
 		SpringApplication.run(Auctionweb5Application.class, args);
+		System.out.println("第1次提交");
+		System.out.println("第二次提交");
 	}
 
 }
